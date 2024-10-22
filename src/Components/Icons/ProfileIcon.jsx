19ff4efe1289dc5c6805784581a230a5fd5f9c91
basantsaed2 <basantsaed718@gmail.com>
2024-10-23 00:29:28 +0300

    fcommit

---
+++ src/Components/Icons/ProfileIcon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeIcon = ({ Width = "24", Height = "24", isActive = '#fff' }) => {
+    return (
+        <svg width={Width} height={Height} viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M8 24C9.21011 21.6419 11.8623 20.0279 16 20.0279C20.1377 20.0279 22.7899 21.6419 24 24M19.2868 11.84C19.2868 13.9608 17.8153 15.68 16 15.68C14.1847 15.68 12.7132 13.9608 12.7132 11.84C12.7132 9.71923 14.1847 8 16 8C17.8153 8 19.2868 9.71923 19.2868 11.84Z" stroke={isActive ? '#1A237E': '#fff'} stroke-width="2" stroke-linecap="round"/>
+        </svg>
+    )
+}
+
+export default HomeIcon
---
+++ src/Routes/UserRoutes.jsx
@@ -0,0 +1,4 @@
+import { Navigate, Outlet, createBrowserRouter } from "react-router-dom";
+import ProtectedRoute from "./ProtectedData/ProtectedRoute";
+import App from "./App";
+// import {UserDashboard}
