6bb7d95274fda747e93f446e7195ee80c9c9d52d
basantsaed2 <basantsaed718@gmail.com>
2024-10-23 10:35:45 +0300

    fcommit

--- src/Components/Icons/AllIcons.js
+++ src/Components/Icons/AllIcons.js
@@ -1,2 +1,10 @@
+//Export All Icons
+
 export { default as HomeIcon } from "./HomeIcon";
 export { default as ProfileIcon } from "./ProfileIcon";
+export { default as StoreIcon } from "./StoreIcon";
+export { default as SubscriptionIcon } from "./SubscriptionIcon";
+export { default as DomainIcon } from "./DomainIcon";
+export { default as ExtraIcon } from "./ExtraIcon";
+export { default as PaymentIcon } from "./PaymentIcon";
+export { default as TutorialIcon } from "./TutorialIcon";
--- src/Components/UserComponents/MenuSideUser.jsx
+++ src/Components/UserComponents/MenuSideUser.jsx
@@ -1,5 +1,5 @@
 import React, { useState ,useEffect} from "react";
-import {HomeIcon,ProfileIcon} from "../Icons/AllIcons";
+import {HomeIcon,ProfileIcon,StoreIcon,SubscriptionIcon,DomainIcon,ExtraIcon,PaymentIcon,TutorialIcon} from "../Icons/AllIcons";
 import { NavLink } from "react-router-dom";
 import { useAuth } from "../../Context/Auth";
 import { Link, useNavigate } from 'react-router-dom'
@@ -13,8 +13,13 @@ const MenuSide = () => {
 
        const [isActiveHome, setIsActiveHome] =  useState(savedState.isActiveHome ?? true);
        const [isActiveProfile, setIsActiveProfile] =  useState(savedState.isActiveProfile ?? false);
-
-     
+       const [isActiveStore, setIsActiveStore] =  useState(savedState.isActiveStore ?? false);
+       const [isActiveSubscription, setIsActiveSubscription] =  useState(savedState.isActiveSubscription ?? false);
+       const [isActiveDomain, setIsActiveDomain] =  useState(savedState.isActiveDomain ?? false);
+       const [isActiveExtra, setIsActiveExtra] =  useState(savedState.isActiveExtra ?? false);
+       const [isActivePayment, setIsActivePayment] =  useState(savedState.isActivePayment ?? false);
+       const [isActiveTutorial, setIsActiveTutorial] =  useState(savedState.isActiveTutorial ?? false);
+   
     //    const handleLogout = () => {
     //           auth.logout();
     //           // navigate("/", { replace: true });
@@ -22,20 +27,95 @@ const MenuSide = () => {
 
        useEffect(() => {
               const sidebarUserState = {
-                     isActiveHome,isActiveProfile
+                     isActiveHome,isActiveProfile,isActiveStore,isActiveSubscription,
+                     isActiveDomain,isActiveExtra,isActivePayment,isActiveTutorial
               };
               localStorage.setItem('sidebarUserState', JSON.stringify(sidebarUserState));
-       }, [isActiveHome,isActiveProfile]);
+       }, [isActiveHome,isActiveProfile,isActiveStore,isActiveSubscription,
+           isActiveDomain,isActiveExtra,isActivePayment,isActiveTutorial
+       ]);
 
        const handleClickHome = () => {
               setIsActiveHome(true);
               setIsActiveProfile(false)
+              setIsActiveStore(false)
+              setIsActiveSubscription(false)
+              setIsActiveDomain(false)
+              setIsActiveExtra(false)
+              setIsActivePayment(false)
+              setIsActiveTutorial(false)
+
        };
        const handleClickProfile = () => {
               setIsActiveHome(false);
               setIsActiveProfile(true)
+              setIsActiveStore(false)
+              setIsActiveSubscription(false)
+              setIsActiveDomain(false)
+              setIsActiveExtra(false)
+              setIsActivePayment(false)
+              setIsActiveTutorial(false)
        };
-
+       const handleClickStore =() =>{
+              setIsActiveHome(false);
+              setIsActiveProfile(false)
+              setIsActiveStore(true)
+              setIsActiveSubscription(false)
+              setIsActiveDomain(false)
+              setIsActiveExtra(false)
+              setIsActivePayment(false)
+              setIsActiveTutorial(false)
+       }
+       const handleClickSubscription =() =>{
+              setIsActiveHome(false);
+              setIsActiveProfile(false)
+              setIsActiveStore(false)
+              setIsActiveSubscription(true)
+              setIsActiveDomain(false)
+              setIsActiveExtra(false)
+              setIsActivePayment(false)
+              setIsActiveTutorial(false)
+       }
+       const handleClickDomain =() =>{
+              setIsActiveHome(false);
+              setIsActiveProfile(false)
+              setIsActiveStore(false)
+              setIsActiveSubscription(false)
+              setIsActiveDomain(true)
+              setIsActiveExtra(false)
+              setIsActivePayment(false)
+              setIsActiveTutorial(false)
+       }
+       const handleClickExtra =() =>{
+              setIsActiveHome(false);
+              setIsActiveProfile(false)
+              setIsActiveStore(false)
+              setIsActiveSubscription(false)
+              setIsActiveDomain(false)
+              setIsActiveExtra(true)
+              setIsActivePayment(false)
+              setIsActiveTutorial(false)
+       }
+       const handleClickPayment =() =>{
+              setIsActiveHome(false);
+              setIsActiveProfile(false)
+              setIsActiveStore(false)
+              setIsActiveSubscription(false)
+              setIsActiveDomain(false)
+              setIsActiveExtra(false)
+              setIsActivePayment(true)
+              setIsActiveTutorial(false)
+       }
+       const handleClickTutorial =() =>{
+              setIsActiveHome(false);
+              setIsActiveProfile(false)
+              setIsActiveStore(false)
+              setIsActiveSubscription(false)
+              setIsActiveDomain(false)
+              setIsActiveExtra(false)
+              setIsActivePayment(false)
+              setIsActiveTutorial(true)
+       }
        return (
               <>
                      <div className="w-full h-full mt-3 flex justify-center mb-10">
@@ -47,6 +127,30 @@ const MenuSide = () => {
                                    <Link to="profile" onClick={handleClickProfile} className={`${isActiveProfile ? 'active' : ''} w-full flex items-center justify-start px-0 py-2 gap-x-5`}>
                                           <ProfileIcon isActive={isActiveProfile} />
                                           <span className={`${isActiveProfile ? "text-mainColor" : "text-secoundColor"} text-xl font-medium`}>Profile</span>
+                                   </Link>
+                                   <Link to="store" onClick={handleClickStore} className={`${isActiveStore ? 'active' : ''} w-full flex items-center justify-start px-0 py-2 gap-x-5`}>
+                                          <StoreIcon isActive={isActiveStore} />
+                                          <span className={`${isActiveStore ? "text-mainColor" : "text-secoundColor"} text-xl font-medium`}>Store</span>
+                                   </Link>
+                                   <Link to="subscription" onClick={handleClickSubscription} className={`${isActiveSubscription ? 'active' : ''} w-full flex items-center justify-start px-0 py-2 gap-x-5`}>
+                                          <SubscriptionIcon isActive={isActiveSubscription} />
+                                          <span className={`${isActiveSubscription ? "text-mainColor" : "text-secoundColor"} text-xl font-medium`}>Subscriptions</span>
+                                   </Link>                      
+                                   <Link to="domain" onClick={handleClickDomain} className={`${isActiveDomain ? 'active' : ''} w-full flex items-center justify-start px-0 py-2 gap-x-5`}>
+                                          <DomainIcon isActive={isActiveDomain} />
+                                          <span className={`${isActiveDomain ? "text-mainColor" : "text-secoundColor"} text-xl font-medium`}>Domain</span>
+                                   </Link>
+                                   <Link to="extra" onClick={handleClickExtra} className={`${isActiveExtra ? 'active' : ''} w-full flex items-center justify-start px-0 py-2 gap-x-5`}>
+                                          <ExtraIcon isActive={isActiveExtra} />
+                                          <span className={`${isActiveExtra ? "text-mainColor" : "text-secoundColor"} text-xl font-medium`}>Extras</span>
+                                   </Link>
+                                   <Link to="payment_history" onClick={handleClickPayment} className={`${isActivePayment ? 'active' : ''} w-full flex items-center justify-start px-0 py-2 gap-x-5`}>
+                                          <PaymentIcon isActive={isActivePayment} />
+                                          <span className={`${isActivePayment ? "text-mainColor" : "text-secoundColor"} text-xl font-medium`}>Payment History</span>
+                                   </Link>
+                                   <Link to="tutorial" onClick={handleClickTutorial} className={`${isActiveTutorial ? 'active' : ''} w-full flex items-center justify-start px-0 py-2 gap-x-5`}>
+                                          <TutorialIcon isActive={isActiveTutorial} />
+                                          <span className={`${isActiveTutorial ? "text-mainColor" : "text-secoundColor"} text-xl font-medium`}>Tutorial</span>
                                    </Link>
                                              
                             </div>
