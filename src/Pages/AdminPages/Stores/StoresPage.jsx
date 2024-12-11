52c654d707f369608640b467f56542a384407ae9
basantsaed2 <basantsaed718@gmail.com>
2024-12-11 12:45:41 +0200

    commit

--- src/Pages/AdminPages/Stores/StoresPage.jsx
+++ src/Pages/AdminPages/Stores/StoresPage.jsx
@@ -27,7 +27,7 @@ const StoresPage = () => {
                });
                if (response.status === 200) {
                       console.log(response.data)
-                    //   setPayments(response.data.payment)
+                      setStores(response.data.stores)
                }
         } catch (error) {
                console.error('Error fetching data:', error);
@@ -95,9 +95,9 @@ const StoresPage = () => {
         );
     }    
       
-    // if (!payments) {
-    //     return <div className='text-mainColor text-2xl font-bold w-full h-full flex items-center justify-center'>No Payment Method data available</div>;
-    // }
+    if (!stores) {
+        return <div className='text-mainColor text-2xl font-bold w-full h-full flex items-center justify-center'>No Stores data available</div>;
+    }
 
        return (
               <>
@@ -112,15 +112,15 @@ const StoresPage = () => {
                     <thead className="w-full">
                         <tr className="w-full border-b-2">
                             <th className="min-w-[80px] sm:w-1/12 lg:w-1/12 text-mainColor text-center font-medium text-sm sm:text-base lg:text-lg xl:text-xl pb-3">#</th>
-                            <th className="min-w-[150px] sm:w-2/12 lg:w-2/12 text-mainColor text-center font-medium text-sm sm:text-base lg:text-lg xl:text-xl pb-3">Title</th>
-                            <th className="min-w-[150px] sm:w-2/12 lg:w-2/12 text-mainColor text-center font-medium text-sm sm:text-base lg:text-lg xl:text-xl pb-3">Image</th>
-                            <th className="min-w-[150px] sm:w-2/12 lg:w-2/12 text-mainColor text-center font-medium text-sm sm:text-base lg:text-lg xl:text-xl pb-3">Description</th>
+                            <th className="min-w-[150px] sm:w-2/12 lg:w-2/12 text-mainColor text-center font-medium text-sm sm:text-base lg:text-lg xl:text-xl pb-3">Store Name</th>
+                            <th className="min-w-[150px] sm:w-2/12 lg:w-2/12 text-mainColor text-center font-medium text-sm sm:text-base lg:text-lg xl:text-xl pb-3">Cpanel Link</th>
+                            <th className="min-w-[150px] sm:w-2/12 lg:w-2/12 text-mainColor text-center font-medium text-sm sm:text-base lg:text-lg xl:text-xl pb-3">Store Link</th>
                             <th className="min-w-[100px] sm:w-1/12 lg:w-1/12 text-mainColor text-center font-medium text-sm sm:text-base lg:text-lg xl:text-xl pb-3">Action</th>
                         </tr>
                     </thead>
-                    {/* <tbody className="w-full">
-                            {payments.map((payment, index) => (
-                                <tr className="w-full border-b-2" key={payment.id}>
+                    <tbody className="w-full">
+                            {stores.map((store, index) => (
+                                <tr className="w-full border-b-2" key={store.id}>
                                         <td
                                                 className="min-w-[80px] sm:min-w-[50px] sm:w-1/12 lg:w-1/12 py-2 text-center text-thirdColor text-sm sm:text-base lg:text-lg xl:text-xl overflow-hidden"
                                         >
@@ -129,84 +129,73 @@ const StoresPage = () => {
                                         <td
                                                 className="min-w-[150px] sm:min-w-[100px] sm:w-2/12 lg:w-2/12 py-2 text-center text-thirdColor text-sm sm:text-base lg:text-lg xl:text-xl overflow-hidden"
                                         >
-                                                {payment?.name || 'Null'}
+                                                {store?.store_name || 'Null'}
                                         </td>
                                         <td
                                                 className="min-w-[150px] sm:min-w-[100px] sm:w-2/12 lg:w-2/12 py-2 text-center text-thirdColor text-sm sm:text-base lg:text-lg xl:text-xl overflow-hidden"
                                         >
-                                                <div className='w-full flex justify-center'>
-                                                <img src={payment?.thumbnailUrl || 'Null'} alt='payment method image' className='w-20 object-content'/>
-                                                </div>
-                                        </td>  
+                                                {store?.link_cbanal || 'Null'}
+                                        </td> 
                                         <td
                                                 className="min-w-[150px] sm:min-w-[100px] sm:w-2/12 lg:w-2/12 py-2 text-center text-thirdColor text-sm sm:text-base lg:text-lg xl:text-xl overflow-hidden"
                                         >
-                                                {payment?.description || 'Null'}
+                                                {store?.link_store || 'Null'}
                                         </td>
 
                                         <td
                                                 className="min-w-[100px] sm:min-w-[80px] sm:w-1/12 lg:w-1/12 py-2 text-center text-thirdColor text-sm sm:text-base lg:text-lg xl:text-xl overflow-hidden"
                                         >
                                                 <div className="flex items-center justify-center gap-x-3">
-                                                <Link to={`edit/${payment.id}`} state={payment.id} type="button">
+                                                <Link to={`edit/${store.id}`} state={store.id} type="button">
                                                         <EditIcon />
                                                 </Link>
-
-                                                {
-                                                        payment.name !== "paymob" && payment.name !== "Paymob" ? 
-                                                        (
-                                                        <>
-                                                        <button type="button" onClick={() => handleOpenDialog(payment.id)}>
-                                                                <DeleteIcon />
-                                                        </button>
-                                                        {openDialog === payment.id && (
-                                                                <Dialog open={true} onClose={handleCloseDialog} className="relative z-10">
-                                                                        <DialogBackdrop className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-                                                                        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-                                                                                <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-                                                                                        <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:max-w-lg">
-                                                                                        <div className="flex flex-col items-center justify-center bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                                                                                                <Wroning Width='28' Height='28' aria-hidden="true" />
-                                                                                                <div className="flex items-center">
-                                                                                                        <div className="mt-2 text-center">
-                                                                                                                <DialogTitle as="h3" className="text-xl font-semibold leading-10 text-gray-900">
-                                                                                                                        You will delete payment method {payment.name|| "null"}
-                                                                                                                </DialogTitle>
-                                                                                                        </div>
+                                                <button type="button" onClick={() => handleOpenDialog(store.id)}>
+                                                        <DeleteIcon />
+                                                </button>
+                                                {openDialog === store.id && (
+                                                        <Dialog open={true} onClose={handleCloseDialog} className="relative z-10">
+                                                                <DialogBackdrop className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                                                                <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                                                                        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                                                                                <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:max-w-lg">
+                                                                                <div className="flex flex-col items-center justify-center bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                                                                                        <Wroning Width='28' Height='28' aria-hidden="true" />
+                                                                                        <div className="flex items-center">
+                                                                                                <div className="mt-2 text-center">
+                                                                                                        <DialogTitle as="h3" className="text-xl font-semibold leading-10 text-gray-900">
+                                                                                                                You will delete store {store.store_name|| "null"}
+                                                                                                        </DialogTitle>
                                                                                                 </div>
                                                                                         </div>
-                                                                                        <div className="px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                                                                                                <button
-                                                                                                        type="button"
-                                                                                                        onClick={() => handleDelete(payment.id)}
-                                                                                                        disabled={isDeleting}
-                                                                                                        className="inline-flex w-full justify-center rounded-md bg-mainColor px-6 py-3 text-sm font-semibold text-white shadow-sm sm:ml-3 sm:w-auto"
-                                                                                                >
-                                                                                                        {isDeleting ? <div className="flex w-10 h-5"><Loading /></div> : 'Delete'}
-                                                                                                </button>
-                                                                                                <button
-                                                                                                        type="button"
-                                                                                                        data-autofocus
-                                                                                                        onClick={handleCloseDialog}
-                                                                                                        className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-6 py-3 text-sm font-medium text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 sm:mt-0 sm:w-auto"
-                                                                                                >
-                                                                                                        Cancel
-                                                                                                </button>
-                                                                                        </div>
-                                                                                        </DialogPanel>
                                                                                 </div>
+                                                                                <div className="px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                                                                                        <button
+                                                                                                type="button"
+                                                                                                onClick={() => handleDelete(store.id)}
+                                                                                                disabled={isDeleting}
+                                                                                                className="inline-flex w-full justify-center rounded-md bg-mainColor px-6 py-3 text-sm font-semibold text-white shadow-sm sm:ml-3 sm:w-auto"
+                                                                                        >
+                                                                                                {isDeleting ? <div className="flex w-10 h-5"><Loading /></div> : 'Delete'}
+                                                                                        </button>
+                                                                                        <button
+                                                                                                type="button"
+                                                                                                data-autofocus
+                                                                                                onClick={handleCloseDialog}
+                                                                                                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-6 py-3 text-sm font-medium text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 sm:mt-0 sm:w-auto"
+                                                                                        >
+                                                                                                Cancel
+                                                                                        </button>
+                                                                                </div>
+                                                                                </DialogPanel>
                                                                         </div>
-                                                                </Dialog>
-                                                        )}
-                                                        </>
-                                                        ):
-                                                        null
-                                                }
+                                                                </div>
+                                                        </Dialog>
+                                                )}
                                                 </div>
                                         </td>
                                 </tr>
                             ))}
-                    </tbody> */}
+                    </tbody>
                 </table>
             </div>
               </div>
