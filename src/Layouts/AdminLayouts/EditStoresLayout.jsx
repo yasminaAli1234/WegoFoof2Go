3ceccdb6b6bcda7b72f86160e113f1f60a6e43cc
basantsaed2 <basantsaed718@gmail.com>
2024-12-12 10:24:06 +0200

    commit

--- src/Layouts/AdminLayouts/EditStoresLayout.jsx
+++ src/Layouts/AdminLayouts/EditStoresLayout.jsx
@@ -6,48 +6,48 @@ import Loading from '../../Components/Loading';
 import axios from 'axios';
 import { useAuth } from '../../Context/Auth';
 
-// export const PaymentMethodDataContext = createContext();
+export const StoresDataContext = createContext();
 
 const EditStoresLayout =()=>{
 
     const auth = useAuth();
-    //    const [isLoading, setIsLoading] = useState(false);
-    //    const [allPaymentData, setAllPaymentData] = useState([]);
-    //    const [paymentEdit, setPaymentEdit] = useState(null);
-    //    const { methodId } = useParams();
+       const [isLoading, setIsLoading] = useState(false);
+       const [allStoresData, setAllStoresData] = useState([]);
+       const [storeEdit, setStoreEdit] = useState(null);
+       const { storesId } = useParams();
 
-    //    useEffect(() => {
-    //     const fetchData = async () => {
-    //         setIsLoading(true);
-    //         try {
-    //                const response = await axios.get('https://login.wegostores.com/admin/v1/payment/method/show', {
-    //                       headers: {
-    //                              Authorization: `Bearer ${auth.user.token}`,
-    //                       },
-    //                });
-    //                if (response.status === 200) {
-    //                       console.log(response.data)
-    //                       setAllPaymentData(response.data.payment)
-    //                }
-    //         } catch (error) {
-    //                console.error('Error fetching data:', error);
-    //         } finally {
-    //                setIsLoading(false);
-    //         }
-    //     };
-    // fetchData(); }, []);
+       useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            try {
+                   const response = await axios.get('https://login.wegostores.com/admin/v1/store/show/approve', {
+                          headers: {
+                                 Authorization: `Bearer ${auth.user.token}`,
+                          },
+                   });
+                   if (response.status === 200) {
+                          console.log(response.data)
+                          setAllStoresData(response.data.stores)
+                   }
+            } catch (error) {
+                   console.error('Error fetching data:', error);
+            } finally {
+                   setIsLoading(false);
+            }
+        };
+    fetchData(); }, []);
 
-    // useEffect(() => {
-    //   if (allPaymentData.length > 0 && methodId) {
-    //          const filteredPayment = allPaymentData.find(
-    //          (payment) => payment.id === parseInt(methodId)
-    //          );
-    //          setPaymentEdit(filteredPayment);
-    //   }
-    // }, [allPaymentData, methodId]);
+    useEffect(() => {
+      if (allStoresData.length > 0 && storesId) {
+             const filteredStores = allStoresData.find(
+             (stores) => stores.id === parseInt(storesId)
+             );
+             setStoreEdit(filteredStores);
+      }
+    }, [allStoresData, storesId]);
 
-    // console.log('allPaymentData', allPaymentData); // Logging the whole array
-    // console.log('PaymentEditData', paymentEdit);
+    console.log('allStoresData', allStoresData); // Logging the whole array
+    console.log('storeEditData', storeEdit);
 
     const navigate = useNavigate();
     const handleGoBack = () => {
@@ -56,9 +56,9 @@ const EditStoresLayout =()=>{
     return(
         <>
         <HeaderPageSection handleClick={handleGoBack} name="Edit Store" />
-        {/* <PaymentMethodDataContext.Provider value={paymentEdit}> */}
+        <StoresDataContext.Provider value={storeEdit}>
            <EditStoresPage/>
-        {/* </PaymentMethodDataContext.Provider> */}
+        </StoresDataContext.Provider>
         </>
     )
 }
