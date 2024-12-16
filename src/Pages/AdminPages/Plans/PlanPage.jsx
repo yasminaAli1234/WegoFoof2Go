364ae473b122c1b35b20003ad205e0f654f8e965
basantsaed2 <basantsaed718@gmail.com>
2024-12-16 15:14:16 +0200

    commit

--- src/Pages/AdminPages/Plans/PlanPage.jsx
+++ src/Pages/AdminPages/Plans/PlanPage.jsx
@@ -284,61 +284,56 @@ const PlanPage = () => {
                                       )}
                                   </>
                               )}
+                          </div>      
+                          <div className='p-4'>
+                          <div className='flex gap-x-5 p-4 pb-0'>
+                                                  <Link to={`edit/${plan.id}`} state={plan.id} type="button">
+                                                          <span className='flex text-[#686868] items-center text-2xl underline gap-1'><EditIcon /> Edit</span>
+                                                  </Link>
+                                                  <button type="button" onClick={() => handleOpenDialog(plan.id)}>
+                                                         <span className='flex text-[#686868] items-center text-2xl underline gap-1'><DeleteIcon /> Delete</span>
+                                                  </button>
+                                                  {openDialog === plan.id && (
+                                                          <Dialog open={true} onClose={handleCloseDialog} className="relative z-10">
+                                                                  <DialogBackdrop className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                                                                  <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                                                                          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                                                                                  <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:max-w-lg">
+                                                                                  <div className="flex flex-col items-center justify-center bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                                                                                          <Wroning Width='28' Height='28' aria-hidden="true" />
+                                                                                          <div className="flex items-center">
+                                                                                                  <div className="mt-2 text-center">
+                                                                                                          <DialogTitle as="h3" className="text-xl font-semibold leading-10 text-gray-900">
+                                                                                                                  You will delete Plan {plan.name|| "null"}
+                                                                                                          </DialogTitle>
+                                                                                                  </div>
+                                                                                          </div>
+                                                                                  </div>
+                                                                                  <div className="px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                                                                                          <button
+                                                                                                  type="button"
+                                                                                                  onClick={() => handleDelete(plan.id)}
+                                                                                                  disabled={isDeleting}
+                                                                                                  className="inline-flex w-full justify-center rounded-md bg-mainColor px-6 py-3 text-sm font-semibold text-white shadow-sm sm:ml-3 sm:w-auto"
+                                                                                          >
+                                                                                                  {isDeleting ? <div className="flex w-10 h-5"><Loading /></div> : 'Delete'}
+                                                                                          </button>
+                                                                                          <button
+                                                                                                  type="button"
+                                                                                                  data-autofocus
+                                                                                                  onClick={handleCloseDialog}
+                                                                                                  className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-6 py-3 text-sm font-medium text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 sm:mt-0 sm:w-auto"
+                                                                                          >
+                                                                                                  Cancel
+                                                                                          </button>
+                                                                                  </div>
+                                                                                  </DialogPanel>
+                                                                          </div>
+                                                                  </div>
+                                                          </Dialog>
+                                                  )}
                           </div>
-
-                          
-                        <div className='w-full p-2'>
-                          <div className='flex gap-x-5 pb-0'>
-                            <Link to={`edit/${plan.id}`} state={plan.id} type="button">
-                              <span className='flex text-[#686868] items-center text-2xl underline gap-1'>
-                                <EditIcon /> Edit
-                              </span>
-                            </Link>
-
-                            <button type="button" onClick={() => handleOpenDialog(plan.id)}>
-                              <span className='flex text-[#686868] items-center text-2xl underline gap-1'>
-                                <DeleteIcon /> Delete
-                              </span>
-                            </button>
-
-                            {openDialog === plan.id && (
-                              <Dialog open={true} onClose={handleCloseDialog} className="relative z-10">
-                                <DialogBackdrop className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-                                <div className="flex min-h-full items-center justify-center p-4 text-center sm:p-0">
-                                  <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:max-w-lg">
-                                    <div className="flex flex-col items-center justify-center bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                                      <Wroning Width='28' Height='28' aria-hidden="true" />
-                                      <div className="mt-2 text-center">
-                                        <DialogTitle as="h3" className="text-xl font-semibold leading-10 text-gray-900">
-                                          You will delete plan {plan.name || "null"}
-                                        </DialogTitle>
-                                      </div>
-                                    </div>
-
-                                    <div className="px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                                      <button
-                                        type="button"
-                                        onClick={() => handleDelete(plan.id)}
-                                        disabled={isDeleting}
-                                        className="inline-flex w-full justify-center rounded-md bg-mainColor px-6 py-3 text-sm font-semibold text-white shadow-sm sm:ml-3 sm:w-auto"
-                                      >
-                                        {isDeleting ? <div className="flex w-10 h-5"><Loading /></div> : 'Delete'}
-                                      </button>
-
-                                      <button
-                                        type="button"
-                                        onClick={handleCloseDialog}
-                                        className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-6 py-3 text-sm font-medium text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 sm:mt-0 sm:w-auto"
-                                      >
-                                        Cancel
-                                      </button>
-                                    </div>
-                                  </DialogPanel>
-                                </div>
-                              </Dialog>
-                            )}
-                          </div>
-                        </div>
+                      </div>
      
                       </div>
                   );
